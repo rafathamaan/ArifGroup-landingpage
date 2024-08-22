2cd8c42d01e37fc2dda0ab457f03dcb39074356a
RafathAmaan Mohammad <116374643+rafathamaan@users.noreply.github.com>
2024-08-22 12:41:58 +0530

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,7 +29,7 @@ module.exports = {
 				},
 			},
 			fontFamily: {
-				sans: ["var(--font-inter)", ...defaultTheme.fontFamily.sans],
+				sans: ["Poppins,var(--font-inter)", ...defaultTheme.fontFamily.sans],
 				display: ["var(--font-calsans)"],
 			},
 			backgroundImage: {
