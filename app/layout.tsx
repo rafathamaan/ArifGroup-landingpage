f5eee3461c5cd0a307645fba492717a0ed30835a
RafathAmaan Mohammad <116374643+rafathamaan@users.noreply.github.com>
2024-08-25 22:20:46 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -137,6 +137,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${poppins.className} ${inter.variable} ${calSans.variable}`}>
       <head>
+      <link rel="icon" href="public/favicon.png" type="image/png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <Analytics />
       </head>
