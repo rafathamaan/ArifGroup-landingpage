4bf825476e462479e33ca5357515acc28ebbb6d8
RafathAmaan Mohammad <116374643+rafathamaan@users.noreply.github.com>
2024-08-31 22:02:42 +0530

    Update particles.tsx

--- app/components/particles.tsx
+++ app/components/particles.tsx
@@ -101,8 +101,8 @@ export default function Particles({
 		const size = Math.floor(Math.random() * 3) + 0.1;
 		const alpha = 0;
 		const targetAlpha = parseFloat((Math.random() * 0.6 + 0.1).toFixed(1));
-		const dx = (Math.random() - 0.5) * .3;
-		const dy = (Math.random() - 0.5) * .3;
+		const dx = (Math.random() - 0.5) * .4;
+		const dy = (Math.random() - 0.5) * .4;
 		const magnetism = 0.1 + Math.random() * 4;
 		return {
 			x,
