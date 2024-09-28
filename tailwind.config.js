782d3b4a5c7cdddaeda5b5a0c28d4a5eb14a2851
RafathAmaan Mohammad <116374643+rafathamaan@users.noreply.github.com>
2024-09-29 00:06:15 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,9 +38,10 @@ module.exports = {
 			},
 			animation: {
 				"fade-in": "fade-in 3s ease-in-out forwards",
-				title: "title 3s ease-in-out forwards",
+				
 				"fade-left": "fade-left 3s ease-in-out forwards",
 				"fade-right": "fade-right 3s ease-in-out forwards",
+				title: "title 3s ease-in-out forwards",
 			},
 			keyframes: {
 				"fade-in": {
